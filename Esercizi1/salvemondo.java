7620a3f6e7bc3d2bd30f8fbdac2d32950d33830c
MatteoBisotti <91792805+MatteoBisotti@users.noreply.github.com>
2021-10-02 18:22:49 +0200

    Add files via upload

---
+++ Esercizi1/salvemondo.java
@@ -0,0 +1,10 @@
+public class salvemondo {
+    public static void main (String[] args) {
+        System.out.println("Salve, mondo!");
+
+        String s = "Pippo";
+        String t = s;
+        System.out.println(s + ", " + t);
+        System.out.println(s.toUpperCase());
+    }
+}
